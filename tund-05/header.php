d5ace522e6b7b89a1a390f7db7720285d20a4b6c
Sten Raudmets <stenmr@users.noreply.github.com>
2019-09-30 11:31:56 +0300

    5. tund

---
+++ tund-05/header.php
@@ -0,0 +1,12 @@
+<?php
+    $website = '
+    <!DOCTYPE html>
+    <html lang="et">
+    <head>
+        <meta charset="utf-8">
+        <title>
+            Steni veebileht!
+        </title>
+    </head>
+    ';
+?>
